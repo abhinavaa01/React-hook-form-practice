5819eb7419247b535d4848409aff68f9764dfb7f
abhinavaa01 <agarwalabhinav91@gmail.com>
2025-02-10 17:26:53 +0530

    Add react-hook-form validation and DevTool integration

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import { useCallback, useState } from "react";
 import "./App.css";
 import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 function App() {
   const {
     register,
     handleSubmit,
     watch,
-    formState: { errors },
+    formState: { touchedFields, errors },
+    control
   } = useForm();
-
+  const values = watch([
+    "username",
+    "firstname",
+    "lastname",
+    "email",
+    "password",
+    "confirmPassword",
+    "street",
+    "address2",
+    "city",
+    "state",
+    "zip",
+  ]);
   const onSubmit = (data) => {
     console.log(data);
+    // console.log("fs:", formState);
   };
 
   return (
@@ -26,20 +41,29 @@ function App() {
         <form
           id="newform"
           action="#"
+          onSubmit={handleSubmit(onSubmit)}
           className="needs-validation text-start"
+          noValidate
         >
           <div className="form-group p-1">
             <label htmlFor="username">Username</label>
             <input
               type="text"
-              className="form-control is-valid"
+              className={
+                touchedFields.username
+                  ? errors.username || !values.username
+                    ? "form-control is-invalid"
+                    : "form-control is-valid"
+                  : "form-control"
+              }
               id="username"
               name="username"
-              required={true}
+              {...register("username", {
+                required: "Username cannot be blank or have spaces.",
+              })}
+              aria-invalid={errors.username ? "true" : "false"}
             />
-            <div className="invalid-feedback">
-              Username cannot be blank or have spaces.
-            </div>
+            <div className="invalid-feedback">{errors.username?.message}</div>
             <div className="valid-feedback">Looks Good</div>
           </div>
 
@@ -49,14 +73,25 @@ function App() {
                 <label htmlFor="firstname">First Name</label>
                 <input
                   type="text"
-                  className="form-control"
+                  className={
+                    touchedFields.firstname
+                      ? errors.firstname
+                        ? "form-control is-invalid"
+                        : "form-control is-valid"
+                      : "form-control"
+                  }
                   id="firstname"
                   name="firstname"
-                  required={true}
+                  {...register("firstname", {
+                    required: "Please enter your first name.",
+                    pattern: {
+                      value: /^[A-Za-z]+$/,
+                      message: "Only alphabets allowed",
+                    },
+                  })}
+                  aria-invalid={errors.firstname ? "true" : "false"}
                 />
-                <div className="invalid-feedback">
-                  Please enter your first name.
-                </div>
+                <div className="invalid-feedback">Please enter your First name.</div>
               </div>
             </div>
             <div className="col-md-6 col-12">
@@ -64,10 +99,17 @@ function App() {
                 <label htmlFor="lastname">Last Name</label>
                 <input
                   type="text"
-                  className="form-control"
+                  className={
+                    touchedFields.lastname
+                      ? errors.lastname
+                        ? "form-control is-invalid"
+                        : "form-control is-valid"
+                      : "form-control"
+                  }
                   id="lastname"
                   name="lastname"
-                  required={true}
+                  {...register("lastname", { required: "Please enter your last name." })}
+                  aria-invalid={errors.lastname ? "true" : "false"}
                 />
                 <div className="invalid-feedback">
                   Please enter your last name.
@@ -79,10 +121,19 @@ function App() {
             <label htmlFor="email">Email</label>
             <input
               type="email"
-              className="form-control"
+              className={
+                touchedFields.email
+                  ? errors.email
+                    ? "form-control is-invalid"
+                    : "form-control is-valid"
+                  : "form-control"
+              }
               name="email"
+              {...register("email", { required: "Please enter a valid email address.", pattern: {
+                value: /^[a-zA-Z0-9. !#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/, message: "Please enter a valid email address."
+              } })}
+              aria-invalid={errors.email ? "true" : "false"}
               id="email"
-              required={true}
             />
             <div className="invalid-feedback">
               Please enter a valid email address.
@@ -96,10 +147,19 @@ function App() {
                 <div className="position-relative">
                   <input
                     type="text"
-                    className="form-control"
+                    className={
+                      touchedFields.password
+                        ? errors.password
+                          ? "form-control is-invalid"
+                          : "form-control is-valid"
+                        : "form-control"
+                    }
                     name="password"
+                    {...register("password", { required: true, pattern :{
+                      value: `/?=.*?[A-Z]/`, message: "At least One capital letter should be present."
+                    } })}
+                    aria-invalid={errors.password ? "true" : "false"}
                     id="password"
-                    required={true}
                   />
                   <i
                     className="position-absolute top-50 end-0 me-2"
@@ -109,6 +169,7 @@ function App() {
                     🙈
                   </i>
                   <div className="invalid-feedback">
+                    {errors.password?.message}
                     Password must contain at least one capital letter, one
                     number, and one special character.
                   </div>
@@ -120,10 +181,17 @@ function App() {
                 <label htmlFor="confirmPassword">Confirm Password</label>
                 <input
                   type="text"
-                  className="form-control"
+                  className={
+                    touchedFields.confirmPassword
+                      ? errors.confirmPassword
+                        ? "form-control is-invalid"
+                        : "form-control is-valid"
+                      : "form-control"
+                  }
                   name="confirmPassword"
+                  {...register("confirmPassword", { required: true })}
+                  aria-invalid={errors.confirmPassword ? "true" : "false"}
                   id="confirmPassword"
-                  required={true}
                 />
                 <div className="invalid-feedback">Passwords do not match.</div>
               </div>
@@ -172,10 +240,15 @@ function App() {
               </div>
               <input
                 type="tel"
-                className="form-control"
+                className={
+                  touchedFields.phone
+                    ? errors.phone
+                      ? "form-control is-invalid"
+                      : "form-control is-valid"
+                    : "form-control"
+                }
                 id="phone"
                 name="phone"
-                required={true}
                 autoComplete="off"
                 placeholder="081234 56789"
                 data-intl-tel-input-id="0"
@@ -190,19 +263,34 @@ function App() {
             <label htmlFor="street">Street Address</label>
             <input
               type="text"
-              className="form-control"
+              className={
+                touchedFields.street
+                  ? errors.street
+                    ? "form-control is-invalid"
+                    : "form-control is-valid"
+                  : "form-control"
+              }
               id="street"
               name="street"
-              required={true}
+              {...register("street", { required: true })}
+              aria-invalid={errors.street ? "true" : "false"}
             />
           </div>
           <div className="form-group p-1">
             <label htmlFor="address2">Address Line 2</label>
             <input
               type="text"
-              className="form-control"
+              className={
+                touchedFields.address2
+                  ? errors.address2
+                    ? "form-control is-invalid"
+                    : "form-control is-valid"
+                  : "form-control"
+              }
               id="address2"
               name="address2"
+              {...register("address2")}
+              aria-invalid={errors.address2 ? "true" : "false"}
               autoComplete="off"
             />
           </div>
@@ -210,10 +298,17 @@ function App() {
             <label htmlFor="city">City</label>
             <input
               type="text"
-              className="form-control"
+              className={
+                touchedFields.city
+                  ? errors.city
+                    ? "form-control is-invalid"
+                    : "form-control is-valid"
+                  : "form-control"
+              }
               id="city"
               name="city"
-              required={true}
+              {...register("city", { required: true })}
+              aria-invalid={errors.city ? "true" : "false"}
             />
             <div className="invalid-feedback">Please enter your city.</div>
           </div>
@@ -221,10 +316,17 @@ function App() {
             <label htmlFor="state">State</label>
             <input
               type="text"
-              className="form-control"
+              className={
+                touchedFields.state
+                  ? errors.state
+                    ? "form-control is-invalid"
+                    : "form-control is-valid"
+                  : "form-control"
+              }
               id="state"
               name="state"
-              required={true}
+              {...register("state", { required: true })}
+              aria-invalid={errors.state ? "true" : "false"}
             />
             <div className="invalid-feedback">Please enter your state.</div>
           </div>
@@ -232,10 +334,17 @@ function App() {
             <label htmlFor="zip">Postal / Zip Code</label>
             <input
               type="text"
-              className="form-control"
+              className={
+                touchedFields.zip
+                  ? errors.zip
+                    ? "form-control is-invalid"
+                    : "form-control is-valid"
+                  : "form-control"
+              }
               id="zip"
               name="zip"
-              required={true}
+              {...register("zip", { required: true })}
+              aria-invalid={errors.zip ? "true" : "false"}
             />
             <div className="invalid-feedback">
               Please enter your postal/zip code.
@@ -243,12 +352,7 @@ function App() {
           </div>
 
           <div className="form-group form-check">
-            <input
-              type="checkbox"
-              className="form-check-input"
-              id="terms"
-              required={true}
-            />
+            <input type="checkbox" className={touchedFields.agreement? errors.agreement? "form-check-input is-invalid" : "form-check-input is-valid" : "form-check-input"} id="terms" {...register("agreement", { required: true})} />
             <label className="form-check-label" htmlFor="terms">
               I accept the <a href="#">Terms and Conditions</a>
             </label>
@@ -260,6 +364,7 @@ function App() {
           </button>
         </form>
       </div>
+      <DevTool control={control} />
     </div>
   );
 }
