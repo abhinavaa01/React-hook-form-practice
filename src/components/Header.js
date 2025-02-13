61eb0564320dafd5e7075092a4b07be90cc0d376
abhinavaa01 <agarwalabhinav91@gmail.com>
2025-02-13 23:44:08 +0530

    Logout added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { auth } from "../service.js/config.firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import { NavLink } from "react-router";
@@ -6,6 +6,11 @@ import { authCustomApi } from "../service.js";
 
 const Header = () => {
   const [user, setUser] = useState(authCustomApi.returnCurrentUser());
+
+  const logOut = () => {
+    localStorage.removeItem("authUser");
+    localStorage.setItem("isloggedIn", 'false')
+  }
 
   onAuthStateChanged(auth, (user) => {
     if (user) {
@@ -15,18 +20,23 @@ const Header = () => {
     }
   });
 
-  const logoutHandler = (e) => {
-    e.preventDefault();
-    authCustomApi.logout();
-  };
+  // const logoutHandler = (e) => {
+  //   e.preventDefault();
+  //   authCustomApi.logout();
+  // };
 
   return (
     <div className="bg-info px-3 py-2 d-flex">
-        <NavLink className={({ isActive }) =>
-            isActive
-              ? "btn btn-dark me-3 my-auto"
-              : "btn btn-outline-dark me-3 my-auto"
-          } to='/'>Home</NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive
+            ? "btn btn-dark me-3 my-auto"
+            : "btn btn-outline-dark me-3 my-auto"
+        }
+        to="/"
+      >
+        Home
+      </NavLink>
       <div id="authStateInHeader" className="me-3 my-auto">
         <span className="fw-bold">Auth State : </span>
         {user
@@ -36,7 +46,7 @@ const Header = () => {
       {user ? (
         <button
           className="btn btn-dark my-auto ms-auto"
-          onClick={logoutHandler}
+          onClick={logOut}
         >
           LOGOUT
         </button>
