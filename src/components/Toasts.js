f1efac6bc4d331e5116c3931f9a2c582c327afe3
abhinavaa01 <agarwalabhinav91@gmail.com>
2025-02-17 18:14:56 +0530

    Enhance Toast notifications: add error and loading messages, and improve styling for better user feedback

--- src/components/Toasts.js
+++ src/components/Toasts.js
@@ -9,26 +9,70 @@ const Toasts = () => {
 
 
   return (
-    <div id="toasts" className="position-fixed end-0 bottom-0 me-4 mb-4 bg-success border-0 rounded-2">
+    <div id="toasts" className="position-fixed end-0 bottom-0 me-4 mb-4 border-0 rounded-2">
       {successMsg && <Toast
         show={successMsg ? true : false}
-        class="toast align-items-center bg-success bg-opacity-75"
+        className="toast align-items-center bg-success bg-opacity-75 my-2"
         role="alert"
         aria-live="assertive"
         aria-atomic="true"
         data-bs-animation="true"
       >
-        <div class="d-flex">
-          <div class="toast-body">{successMsg}</div>
+        <div className="d-flex">
+          <div className="toast-body">{successMsg}</div>
           <button
             type="button"
-            class="btn-close me-2 m-auto"
+            className="btn-close me-2 m-auto"
             data-bs-dismiss="toast"
             aria-label="Close"
           ></button>
         </div>
       </Toast>}
-    </div>
+
+      
+      {errorMsg && <Toast
+        show={errorMsg ? true : false}
+        className="toast align-items-center bg-danger my-2 bg-opacity-75"
+        role="alert"
+        aria-live="assertive"
+        aria-atomic="true"
+        data-bs-animation="true"
+      >
+        <div className="d-flex">
+          <div className="toast-body">{errorMsg}</div>
+          <button
+            type="button"
+            className="btn-close me-2 m-auto"
+            data-bs-dismiss="toast"
+            aria-label="Close"
+          ></button>
+        </div>
+      </Toast>}
+
+      
+      {loadingMsg && <Toast
+        show={errorMsg ? true : false}
+        className="toast align-items-center bg-warning my-2 bg-opacity-75"
+        role="alert"
+        aria-live="assertive"
+        aria-atomic="true"
+        data-bs-animation="true"
+      >
+        <div className="d-flex">
+          <div className="toast-body">
+            <span className="spinner-border spinner-border-sm text-warning"></span>
+            {loadingMsg? loadingMsg : "Loading..."}
+            </div>
+          <button
+            type="button"
+            className="btn-close me-2 m-auto"
+            data-bs-dismiss="toast"
+            aria-label="Close"
+          ></button>
+        </div>
+      </Toast>}
+
+      </div>
   );
 };
 
